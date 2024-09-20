822e5fde40107437bf31f320aba9dc12cdd0f2d8
Leandro <63678585+Liuth16@users.noreply.github.com>
2024-09-20 17:03:26 -0300

    JS files with dynamic page creation

---
+++ src/home.js
@@ -0,0 +1,9 @@
+function createHome (){
+    const mainDiv = document.createElement("div");
+    mainDiv.classList.add("main");
+    const titleDiv = document.createElement("div");
+    const imageDiv = document.createElement("div");
+    imageDiv.classList.add("image-container");
+    const descriptionDiv = document.createElement("div");
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
+import "./styles.css";
+
+
 console.log("Working fine")
-alert("Working fine!!")
